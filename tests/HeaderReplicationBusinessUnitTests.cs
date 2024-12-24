2f1d33cb8f1db6102d7dfda74fc9818891514521
Burak Tungut <buraktungut@gmail.com>
2024-12-25 00:11:56 +0300

    Add unit test to validate ArgumentNullException for null request headers in HeaderReplicationBusiness

--- tests/HeaderReplicationBusinessUnitTests.cs
+++ tests/HeaderReplicationBusinessUnitTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Xunit;
 
 namespace DotNetHeaderReplicator.Tests;
@@ -38,6 +39,21 @@ public class HeaderReplicationBusinessUnitTests
         // Assert
         Assert.Throws<ArgumentNullException>(act);
     }
+
+    [Fact]
+    public void HeaderReplicationBusiness_Should_ThrowArgumentNullException_WhenRequestHeadersIsNull()
+    {
+        // Arrange
+        var business = new HeaderReplicationBusiness(false, GetEmptyEnumerable(), GetEmptyEnumerable());
+        IHeaderDictionary requestHeaders = null;
+
+        // Act
+        Action act = () => business.GetReplicatedHeaders(requestHeaders);
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(act);
+    }
+
 
 
 }
