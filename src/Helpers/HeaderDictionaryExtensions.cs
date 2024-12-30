7edb3de326f7421638692542430c89bfa7be63f4
Burak Tungut <buraktungut@gmail.com>
2024-12-31 02:38:46 +0300

    Change AddOrReplaceRange method to internal visibility in HeaderDictionaryExtensions

--- src/Helpers/HeaderDictionaryExtensions.cs
+++ src/Helpers/HeaderDictionaryExtensions.cs
@@ -4,7 +4,7 @@ namespace DotNetHeaderReplicator;
 
 internal static class HeaderDictionaryExtensions
 {
-    public static void AddOrReplaceRange(this IHeaderDictionary dic, IHeaderDictionary source)
+    internal static void AddOrReplaceRange(this IHeaderDictionary dic, IHeaderDictionary source)
     {
         if (dic == null) throw new ArgumentNullException(nameof(dic));
         if (source == null) throw new ArgumentNullException(nameof(source));
