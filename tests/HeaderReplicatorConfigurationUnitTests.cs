8a99fb583f17261c5a5bce10b65cc731fa4a410a
Burak Tungut <buraktungut@gmail.com>
2024-12-31 03:02:12 +0300

    Disable nullable reference types and add unit tests for HeaderReplicatorConfiguration constructor validation

---
+++ tests/HeaderReplicatorConfigurationUnitTests.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Castle.Core.Logging;
+using DotNetHeaderReplicator.Internals;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using Xunit;
+
+namespace DotNetHeaderReplicator.Tests;
+
+public class HeaderReplicatorConfigurationUnitTests
+{
+    [Fact]
+    public void Ctor_ShouldThrowException_WhenAllowAllIsFalse_And_AllowedPrefixesIsNull()
+    {
+        // Arrange
+        bool isAllowAll = false;
+        HashSet<string> allowedPrefixes = null;
+        HashSet<string> ignoredSentences = new HashSet<string>(Helpers.GetEmptyEnumerable());
+
+        // Act
+        Action act = () => new HeaderReplicatorConfiguration(isAllowAll, allowedPrefixes, ignoredSentences);
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(act);
+    }
+
+    [Fact]
+    public void Ctor_ShouldThrowException_WhenAllowAllIsFalse_And_IgnoredSentencesIsNull()
+    {
+        // Arrange
+        bool isAllowAll = false;
+        HashSet<string> allowedPrefixes = new HashSet<string>(Helpers.GetEmptyEnumerable()); ;
+        HashSet<string> ignoredSentences = null;
+
+        // Act
+        Action act = () => new HeaderReplicatorConfiguration(isAllowAll, allowedPrefixes, ignoredSentences);
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(act);
+    }
+
+    [Fact]
+    public void Ctor_ShouldThrowException_WhenAllowAllIsTrue_And_AllowedPrefixesIsEmpty()
+    {
+        // Arrange
+        bool isAllowAll = true;
+        HashSet<string> allowedPrefixes = new HashSet<string>(Helpers.GetEmptyEnumerable());
+        HashSet<string> ignoredSentences = new HashSet<string>
+        {
+            Helpers.GetRandomValue(),
+            Helpers.GetRandomValue(),
+            Helpers.GetRandomValue()
+        };
+
+        // Act
+        Action act = () => new HeaderReplicatorConfiguration(isAllowAll, allowedPrefixes, ignoredSentences);
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(act);
+    }
+
+    [Fact]
+    public void Ctor_ShouldThrowException_WhenAllowAllIsTrue_And_IgnoredSentencesIsEmpty()
+    {
+        // Arrange
+        bool isAllowAll = true;
+        HashSet<string> allowedPrefixes = new HashSet<string>
+        {
+            Helpers.GetRandomValue(),
+            Helpers.GetRandomValue(),
+            Helpers.GetRandomValue()
+        };
+        HashSet<string> ignoredSentences = new HashSet<string>(Helpers.GetEmptyEnumerable());
+
+        // Act
+        Action act = () => new HeaderReplicatorConfiguration(isAllowAll, allowedPrefixes, ignoredSentences);
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(act);
+    }
+
+    [Fact]
+    public void Ctor_ShouldThrowException_WhenAllowAllIsTrue_And_AllowedPrefixesAndIgnoredSentencesAreNotEmpty()
+    {
+        // Arrange
+        bool isAllowAll = true;
+        HashSet<string> allowedPrefixes = new HashSet<string>
+        {
+            Helpers.GetRandomValue(),
+            Helpers.GetRandomValue(),
+            Helpers.GetRandomValue()
+        };
+        HashSet<string> ignoredSentences = new HashSet<string>
+        {
+            Helpers.GetRandomValue(),
+            Helpers.GetRandomValue(),
+            Helpers.GetRandomValue()
+        };
+
+        // Act
+        Action act = () => new HeaderReplicatorConfiguration(isAllowAll, allowedPrefixes, ignoredSentences);
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(act);
+    }
+
+}
